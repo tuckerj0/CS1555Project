b0b269a24deea06ef057d69060f9a92bd79054b7
tuckerj0 <jat134@pitt.edu>
2016-11-17 17:35:51 -0500

    Update pittToursDB.sql

--- pittToursDB.sql
+++ pittToursDB.sql
@@ -121,6 +121,67 @@ set new.cost = new_price
 END;
 /
 
-commit;
 
---start pittToursData.sql
+--procedure
+create or replace function biggerPlane (plane_capacity in int) return char (4)
+as
+begin try
+if (Plane.plane_capacity > plane_capacity and Plane.airline_id = Flight.airline_id)
+	return Plane.plane_type;
+end try
+begin catch 
+	RAISERROR ('SORRY NO LARGER PLANE') WITH NOWAIT;
+end;
+/
+
+create or replace function smallerPlane (plane_capacity in int) return char (4)
+as
+begin try
+if (Plane.plane_capacity < plane_capacity and Plane.airline_id = Flight.airline_id)
+	return Plane.plane_type;
+end try
+begin catch
+	RAISERROR ('SORRY NO SMALLER PLANE') WITH NOWAIT;
+end;
+/
+
+-- function
+
+create or replace trigger adjustTicket
+after update Price
+for each row
+begin
+update Reservation
+if (Reservation.reservation_date = Date.c_date) {
+reservation.cost := Reservation.cost + (Price.high_price - new.high_price);
+}
+else {
+reservation.cost := Reservation.cost + (Price.low_price - new.low_price);
+}
+END; / 
+
+--planeUpdate trigger
+ 
+create or replace trigger planeUpgrade
+before update Reservation_detail
+for each row
+when (COUNT(Reservation_detail.reservation_number) > Plane.plane_capacity)
+begin
+Flight.plane_type = largerPlane(:new.plane_capacity);
+where  Reservation_detail.flight_number = Flight.flight_number
+end; /
+
+-- cancel reservation triger
+create or replace cancelReservation
+before update Reservation
+for each row
+declare @dt
+declare @timeDiff
+	@dt = CURRENT_TIME
+	@timeDiff = @dt - departure_time
+	if (timeDiff < 12 and Reservation.ticketed = 'N') 
+			Flight.plane_type = smallerPlane(:new.plane_capacity)
+where Flight.flight_number = Reservation_detail.flight_number
+end;/
+
+start pittToursData.sql
