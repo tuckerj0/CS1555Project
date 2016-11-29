e03fdbafa877f3774142fb333e8d4ad9e30c19ff
vas55 <vas55@pitt.edu>
2016-11-28 21:39:41 -0500

    Update airlineUI.java

--- airlineUI.java
+++ airlineUI.java
@@ -63,34 +63,92 @@ public class airlineUI{
 		}
 	}
 	public static void adminInterface(){
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		while(true){
 			System.out.println("\n-----Administrator Menu-----");
 			System.out.println("1. Erase the database");
-			System.out.println("2. Load ariline information");
-			System.out.println("3. Load scedule information");
+			System.out.println("2. Load airline information");
+			System.out.println("3. Load schedule information");
 			System.out.println("4. Load pricing information");
 			System.out.println("5. Load plane information");
 			System.out.println("6. Generate passenger manifest for a specific flight on a given day");
-			System.out.println("7. Quit\n");
-			int userInput = scanner.nextInt();
+			System.out.println("7. Quit");
+			int userInput = scan.nextInt();
+			
 			if(userInput == 1){
-
+				System.out.println("Are you sure you want to delete the entire database? Please type Y for yes or N for no");
+				Scanner scan1 = new Scanner(System.in);
+				String s = scan1.next();
+				if (s == "Y") {
+					deleteDatabase();
+				}
+				else {
+					System.out.println("You can changed your mind about deleting the database. Goodbye.");
+					System.exit(0);
+				}
+				scan1.close();
 			}
+			
 			else if(userInput == 2){
-
+				try {
+					loadAirlineInformation();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 			else if(userInput == 3){
-
+				try {
+					loadScheduleInformation();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 			else if(userInput == 4){
-
+				Scanner scan1 = new Scanner(System.in);
+				System.out.println("Would you like to load pricing information or change the price of an existing flight? To load price please enter L or if you want to change please enter C");
+				if (scan1.next().equals("C")) {
+					//change pricing information
+					try {
+						changePricingInformation();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				else if (scan1.next().equals("L")) {
+					//load information
+					try {
+						loadPricingInformation();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				else {
+					System.out.println("You have inputted the wrong entry. Sorry, goodbye.");
+					System.exit(0);
+				}
+				scan1.close();
 			}
 			else if(userInput == 5){
-
+				try {
+					loadPlaneInformation();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 			else if(userInput == 6){
-				
+				try {
+					generateManifesto();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 			else if(userInput == 7){
 				try{
@@ -109,8 +167,234 @@ public class airlineUI{
 			else{
 				System.out.println("Invalid input\n");
 			}
+			scan.close();
+
 		}
 	}
+	
+	public static int deleteDatabase () {
+		System.out.println("Deleting Database...");
+		try {
+			Statement stmt = conn.createStatement();
+			String airlineDelete = "TRUNCATE Airline";
+			stmt.executeUpdate(airlineDelete);
+			
+			String flightDelete = "TRUNCATE Flight";
+			stmt.executeUpdate(flightDelete);
+			
+			String planeDelete = "TRUNCATE Plane";
+			stmt.executeUpdate(planeDelete);
+			
+			String priceDelete = "TRUNCATE Price";
+			stmt.executeUpdate(priceDelete);
+			
+			String customerDelete = "TRUNCATE Customer";
+			stmt.executeUpdate(customerDelete);
+			
+			String reservationDelete = "TRUNCATE Reservation";
+			stmt.executeUpdate(reservationDelete);
+			
+			String reservationDetailsDelete = "TRUNCATE Reservation_detail";
+			stmt.executeUpdate(reservationDetailsDelete);
+			
+			String dateDelete = "TRUNCATE Date";
+			stmt.executeUpdate(dateDelete);
+			
+			System.out.println("Database is now empty");
+			
+		} catch (SQLException e) {
+			System.out.println("An error occured while deleting database. Goodbye.");
+			System.exit(0);
+		}
+		return 0;
+	}
+	
+	public static void loadAirlineInformation() throws IOException, SQLException {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Please enter a file name to load airline information");
+		String fileName = scan.next();
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line;
+		System.out.println("Inserting airline info...");
+		while ((line = br.readLine()) != null) {
+			String var[] = line.split(",");
+			String airlineID = var[0];
+			String airlineName = var[1];
+			String airlineAbbreviation = var[2];
+			String yearFounded = var[3];
+			
+			Statement stmt = conn.createStatement();
+			String insertAirline = "INSERT INTO Airline (airline_id, airline_name, airline_abbreviation, year_founded) VALUES ('"+airlineID+"','" +airlineName+"','"+airlineAbbreviation+"','"+yearFounded+"');";
+			stmt.executeUpdate(insertAirline);
+		}
+		br.close();
+		scan.close();
+	}
+	
+	public static void loadScheduleInformation() throws IOException, SQLException {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Please enter a file name to load flight information");
+		String fileName = scan.next();
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line;
+		System.out.println("Inserting flight info...");
+		while ((line = br.readLine()) != null) {
+			String var[] = line.split(",");
+			String flightNumber = var[0];
+			String planeType = var[1];
+			String departureCity = var[2];
+			String arrivalCity = var[3];
+			String departureTime = var[4];
+			String arrivalTime = var[5];
+			String weeklySchedule = var[6];
+			
+			Statement stmt = conn.createStatement();
+			String insertFlight = "INSERT INTO Flight (flight_number, plane_type, departure_city, arrival_city, departure_time, arrival_time, weekly_schedule) VALUES ('"+flightNumber+"','" +planeType+"','"+departureCity+"','"+arrivalCity+"','"+departureTime+"','"+arrivalTime+"','"+weeklySchedule+"');";
+			stmt.executeUpdate(insertFlight);
+		}
+		br.close();
+		scan.close();
+	}
+	
+	public static int changePricingInformation() throws SQLException {
+		Scanner input = new Scanner (System.in);
+		String departureCity;
+		String arrivalCity;
+		String airlineID;
+		int highCost;
+		int lowCost;
+		
+		System.out.println("Please enter departure city as airport code. E.g. Pittsburgh will be PIT");
+		if (input.next().length() > 3) {
+			System.out.println("Sorry you inputted too many letters for the airport code. Goodbye.");
+			return 0;
+		}
+		else {
+			departureCity = input.next();
+		}
+		
+		System.out.println("Please enter arrival city as airport code. E.g. Pittsburgh will be PIT");
+		if (input.next().length() > 3) {
+			System.out.println("Sorry you inputted too many letters for the airport code. Goodbye.");
+			return 0;
+		}
+		else {
+			arrivalCity = input.next();
+		}
+		
+		System.out.println("Please enter airline ID number. The number should not be more than three numbers");
+		if (input.next().length() > 3) {
+			System.out.println("Sorry you inputted too many numbers for the airline ID. Goodbye.");
+			return 0;
+		}
+		else {
+			airlineID = input.next();
+		}
+		
+		System.out.println("Please enter the high cost for this flight. Input should be a max of 3 digits and rounded to the closest ones place. E.g. $230.10 will be 230");
+		if (input.next().length() > 3) {
+			System.out.println("Sorry you inputted too many numbers for the High Cost. Goodbye.");
+			return 0;
+		}
+		else {
+			highCost = input.nextInt();
+		}
+		
+		System.out.println("Please enter the low cost for this flight. Input should be a max of 3 digits and rounded to the closest ones place. E.g. $230.10 will be 230");
+		if (input.next().length() > 3) {
+			System.out.println("Sorry you inputted too many numbers for the airline ID. Goodbye.");
+			return 0;
+		}
+		else {
+			lowCost = input.nextInt();
+		}
+		
+		Statement stmt = conn.createStatement();
+		String updatePrice = "UPDATE PRICE SET high_cost =" + highCost + ",low_cost=" + lowCost + "WHERE departure_city =" + departureCity + " and arrival_city=" + arrivalCity + " and airline_id="+airlineID+ ";";
+		stmt.executeUpdate(updatePrice);
+		input.close();
+		return 0;
+	}
+	
+	public static void loadPricingInformation() throws IOException, SQLException {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Please enter a file name to load pricing information");
+		String fileName = scan.next();
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line;
+		System.out.println("Inserting pricing info...");
+		while ((line = br.readLine()) != null) {
+			String var[] = line.split(",");
+			String departureCity = var[0];
+			String arrivalCity = var[1];
+			String airlineID = var[2];
+			String highPrice = var[3];
+			String lowPrice = var[4];
+			
+			Statement stmt = conn.createStatement();
+			String insertPrice = "INSERT INTO Price (departure_city, arrival_city, airline_id, high_price, low_price) VALUES ('"+departureCity+"','" +arrivalCity+"','"+airlineID+"','"+highPrice+"','"+lowPrice+"');";
+			stmt.executeUpdate(insertPrice);
+		}
+		br.close();
+		scan.close();
+	}
+	
+	public static int loadPlaneInformation() throws IOException, SQLException {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Please enter a file name to load plane information");
+		String fileName = scan.next();
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line;
+		System.out.println("Inserting plane info...");
+		while ((line = br.readLine()) != null) {
+			String var[] = line.split(",");
+			String planeType = var[0];
+			String manufacture = var[1];
+			String planeCapacity = var[2];
+			String lastService = var[3];
+			String year = var[4];
+			String ownerID = var[5];
+			
+			if (planeType.length() > 4) {
+				System.out.println("Plane type input is incorrect.");
+				return 0;
+			}
+			if (ownerID.length() > 3) {
+				System.out.println("Owner id input is incorrect");
+				return 0;
+			}
+			Statement stmt = conn.createStatement();
+			String insertPlane = "INSERT INTO Plane (plane_type, manufacture, plane_capacity, last_service, year, owner_id) VALUES ('"+planeType+"','" +manufacture+"','"+planeCapacity+"','"+lastService+"','"+year+"','"+ownerID+"');";
+			stmt.executeUpdate(insertPlane);
+		}
+		br.close();
+		scan.close();
+		return 0;
+	}
+	
+	public static int generateManifesto() throws SQLException {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Please input the flight number. The flight number should not be more than 3 numbers: ");
+		String flightNumber = scan.next();
+		
+		System.out.println("Please input the date. The format of the date should be MM/DD/YYYY: ");
+		String date = scan.next();
+		
+		if (flightNumber.length() > 3) {
+			System.out.println("Flight number input is incorrect.");
+			return 0;
+		}
+		Statement stmt = conn.createStatement();
+		String manifesto = "Select salutation, first_name, last_name FROM Customer AS c INNER JOIN Reservation as r ON c.cid = r.cid INNER JOIN Reservation_detail as d ON r.reservation_number = d.reservation_number WHERE Reservation_detail.flight_number = " + flightNumber + " and" + "Reservation_detail.flight_date = " + date + ";";
+		ResultSet rs = stmt.executeQuery(manifesto);
+		scan.close();
+		return 0;
+	}
+
+//-------------------------------------------------------------	
+//------------------------ USER INTERFACE ---------------------
+//-------------------------------------------------------------
+	
 	public static void userInterface(){
 		Scanner scanner = new Scanner(System.in);
 		while(true){
@@ -178,9 +462,19 @@ public class airlineUI{
 	}
 	public static void addCustomer(){
 		Scanner scanner = new Scanner(System.in);
-		Boolean correct = true;
-		Boolean verify = true;
-		String salutation = "", fname = "", lname = "", street = "", city= "", state = "", pn = "", email = "", ccn= "", cced  "", cced2 = "";
+		boolean correct = true;
+		boolean verify = true;
+		String salutation = "", 
+				fname = "", 
+				lname = "", 
+				street = "", 
+				city= "", 
+				state = "", 
+				pn = "", 
+				email = "", 
+				ccn= "", 
+				cced= "", 
+				cced2 = "";
 		String sql;
 		try{
 			while(correct){
@@ -195,8 +489,7 @@ public class airlineUI{
 					lname = scanner.nextLine();
 					sql = "SELECT first_name, last_name FROM customer WHERE first_name = "+fname+" AND last_name = " +lname +"";
 					ResultSet rs = stmt.executeQuery(sql);
-					Boolean exist = rs.next();
-					if(exist != null){
+					if(rs.next() == false){
 						System.out.println("A customer by this name already exists.");
 						System.out.println("Please eneter a new name.");
 					}
@@ -288,8 +581,8 @@ public class airlineUI{
 			lname = scanner.nextLine();
 			sql = "SELECT first_name, last_name FROM customer WHERE first_name = "+fname+" AND last_name = " +lname +"";
 			ResultSet rs = stmt.executeQuery(sql);
-			Boolean exist = rs.next();
-			if(exist != null){
+			
+			if(rs.next()){
 				String s  = rs.getString("salutation");
 				int cid = rs.getInt("cid");
 				String pn = rs.getString("phone");
@@ -313,7 +606,7 @@ public class airlineUI{
 		Scanner scanner = new Scanner(System.in);
 		String sql = "";
 		String ca = "";
-		String = "";
+		String cb = "";
 		System.out.println("---Find flight price menu---");System.out.println("Available cities:");
 		try{
 			System.out.println("Please enter the following");
@@ -324,9 +617,8 @@ public class airlineUI{
 			
 			sql = "SELECT high_price, low_price FROM price WHERE departure_city = " + ca + " AND arrival_city = " + cb +"";
 			ResultSet rs = stmt.executeQuery(sql);
-			Boolean AtoB = rs.next();
-			int AtoBhp, AtoBlp;
-			if(AtoB != null){
+			int AtoBhp = 0, AtoBlp = 0;
+			if(rs.next()){
 				AtoBhp  = rs.getInt("high_price");
 				AtoBlp  = rs.getInt("low_price");
 				System.out.println("Flight from "+ca+" to" + cb+ ":");
@@ -337,10 +629,9 @@ public class airlineUI{
 				System.out.println("Flight from "+ca+" to" + cb+ " not available");
 			}
 			sql = "SELECT high_price, low_price FROM price WHERE departure_city = " + cb + " AND arrival_city = " + ca +"";
-			rs = stmt.executeQuery(sql);
-			Boolean BtoA = rs.next();
-			int BtoAhp, BtoAlp;
-			if(BtoA != null){
+			ResultSet rs1 = stmt.executeQuery(sql);
+			int BtoAhp = 0, BtoAlp = 0;
+			if(rs1.next()){
 				BtoAhp  = rs.getInt("high_price");
 				BtoAlp  = rs.getInt("low_price");
 				System.out.println("Flight from "+cb+" to" + ca+ ":");
@@ -350,7 +641,7 @@ public class airlineUI{
 			else{
 				System.out.println("Flight from "+cb+" to" + ca+ " not available");
 			}
-			if(BtoA != null && AtoB != null){
+			if(rs.next() && rs1.next()){
 				System.out.println("Round trip from "+ca+" to" + cb+ ":");
 				int rthp = BtoAhp + AtoBhp;
 				int rtlp = BtoAlp + AtoBlp;
@@ -372,7 +663,6 @@ public class airlineUI{
 		
 	}
 }
-
 
 
 
